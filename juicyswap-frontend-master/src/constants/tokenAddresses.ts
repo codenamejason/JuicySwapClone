657a8b2f0183aacf50aa68b97d2bd0ed0c5b7649
codenamejason <codenamejason@outlook.com>
2020-11-14 12:47:06 -0500

    first commit

---
+++ juicyswap-frontend-master/src/constants/tokenAddresses.ts
@@ -0,0 +1,4 @@
+export const sushi = '0x0e2298E3B3390e3b945a5456fBf59eCc3f55DA16'
+export const sushiv2 = '0xAba8cAc6866B83Ae4eec97DD07ED254282f6aD8A'
+export const sushiAddress = '0x43a7903E3a839a67192151eE300e11198985E54b'
+export const masterChefAddress = '0x245A074cA9814fB46A21562bC70fAB92F8A3F779'
